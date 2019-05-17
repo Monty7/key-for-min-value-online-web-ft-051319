c9447038bf1ba8388f5407be5209db88b47f94bf
M Banks <montensia1@outlook.com>
2019-05-17 16:58:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_val = nil
+  lowest_key = nil
   name_hash.collect do |furniture, value|
-    if value == value 
+    if lowest_val == nil || value < lowest_val
+      lowest_val = value
+      lowest_key = furniture
+    end
   end
 end
